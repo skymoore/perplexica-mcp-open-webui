3e861a89bc8c95372fb986ed4adb288d4bc74a96
Thomas Hessel <thetom42@gmail.com>
2025-06-01 14:24:46 +0000

    Add 'tailscale up' to post-create script (#3)

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,3 +3,5 @@
 # Install uv package manager
 curl -LsSf https://astral.sh/uv/install.sh | sh
 echo 'eval "$(uv generate-shell-completion bash)"' >> ~/.bashrc
+
+sudo tailscale up --accept-routes --auth-key=$TS_AUTH_KEY
